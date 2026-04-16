ff11661fea73d7b8aa081588d0207d800a62eefc
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-04-16 18:17:18 +0700

    Refactor binary search implementation and input handling

--- binarysearch_0055.cpp
+++ binarysearch_0055.cpp
@@ -83,4 +83,23 @@ void binarySearch()
         int low = 0;
         int high = nPanjang - 1;
 
+        do
+        {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[v] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+
+            if (x < element[mid])
+            {
+                low = mid + 1;
+            }
+
         
