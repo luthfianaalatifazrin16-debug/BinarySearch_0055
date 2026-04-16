590eb48125a6bc368851ed1a66dc41bad9d7c37c
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-04-16 18:16:53 +0700

    Refactor binary search program structure

--- binarysearch_0055.cpp
+++ binarysearch_0055.cpp
@@ -72,4 +72,15 @@ void binarySearch()
 {
     char ulang;
     do
-    
+    {
+        cout << "\n================================\n";
+        cout << "     Pencarian Binary Search      \n";
+        cout << "================================\n";
+
+        cout << "Masukan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        
